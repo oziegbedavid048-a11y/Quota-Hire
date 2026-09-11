39251658d39fd1835ddc106898f51f20a25fefc3
oziegbedavid048-a11y <oziegbedavid048-a11y@users.noreply.github.com>
2026-09-11 23:09:49 +0000

    deploy: 04398c4e78b319fa97ddd62956acf9af8b275eaf

--- assets/chevron-right-DstTfXo6.js
+++
@@ -1,6 +0,0 @@
-import{k as o}from"./index-CNUo4_ep.js";/**
- * @license lucide-react v1.32.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],c=o("chevron-right",t);export{c as C};
